2262c2a1026bf13bcce9c1bf041cdca547577e3f
comphone <narinoutagit@gmail.com>
2025-06-25 09:08:47 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -69,4 +69,6 @@ def task_scheduler():
 threading.Thread(target=task_scheduler, daemon=True).start()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
