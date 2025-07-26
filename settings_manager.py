c3188018470d6d6893d973063cb1c199f3a57ff5
comphone <narinoutagit@gmail.com>
2025-07-26 12:30:55 +0700

    ...

--- settings_manager.py
+++ settings_manager.py
@@ -1,45 +1,45 @@
 import os
 import json
 
-SETTINGS\_FILE = 'settings.json'
-\_DEFAULT\_APP\_SETTINGS\_STORE = {
-'report\_times': { 'appointment\_reminder\_hour\_thai': 7, 'outstanding\_report\_hour\_thai': 20, 'customer\_followup\_hour\_thai': 9 },
-'line\_recipients': { 'admin\_group\_id': os.environ.get('LINE\_ADMIN\_GROUP\_ID', ''), 'technician\_group\_id': os.environ.get('LINE\_TECHNICIAN\_GROUP\_ID', ''), 'manager\_user\_id': '' },
-'equipment\_catalog': [],
-'auto\_backup': { 'enabled': False, 'hour\_thai': 2, 'minute\_thai': 0 },
-'shop\_info': { 'contact\_phone': '081-XXX-XXXX', 'line\_id': '@ComphoneService' },
-'technician\_list': []
+SETTINGS_FILE = 'settings.json'
+_DEFAULT_APP_SETTINGS_STORE = {
+    'report_times': { 'appointment_reminder_hour_thai': 7, 'outstanding_report_hour_thai': 20, 'customer_followup_hour_thai': 9 },
+    'line_recipients': { 'admin_group_id': os.environ.get('LINE_ADMIN_GROUP_ID', ''), 'technician_group_id': os.environ.get('LINE_TECHNICIAN_GROUP_ID', ''), 'manager_user_id': '' },
+    'equipment_catalog': [],
+    'auto_backup': { 'enabled': False, 'hour_thai': 2, 'minute_thai': 0 },
+    'shop_info': { 'contact_phone': '081-XXX-XXXX', 'line_id': '@ComphoneService' },
+    'technician_list': []
 }
 
-def get\_app\_settings():
-"""Reads settings from the JSON file, falling back to defaults."""
-app\_settings = json.loads(json.dumps(\_DEFAULT\_APP\_SETTINGS\_STORE))
-if os.path.exists(SETTINGS\_FILE):
-try:
-with open(SETTINGS\_FILE, 'r', encoding='utf-8') as f:
-loaded\_settings = json.load(f)
-for key, default\_value in app\_settings.items():
-if key in loaded\_settings:
-if isinstance(default\_value, dict) and isinstance(loaded\_settings[key], dict):
-app\_settings[key].update(loaded\_settings[key])
-else:
-app\_settings[key] = loaded\_settings[key]
-except (json.JSONDecodeError, IOError):
-\# In case of error, just use the default and overwrite the corrupted file later.
-pass
-return app\_settings
+def get_app_settings():
+    """Reads settings from the JSON file, falling back to defaults."""
+    app_settings = json.loads(json.dumps(_DEFAULT_APP_SETTINGS_STORE))
+    if os.path.exists(SETTINGS_FILE):
+        try:
+            with open(SETTINGS_FILE, 'r', encoding='utf-8') as f:
+                loaded_settings = json.load(f)
+                for key, default_value in app_settings.items():
+                    if key in loaded_settings:
+                        if isinstance(default_value, dict) and isinstance(loaded_settings[key], dict):
+                            app_settings[key].update(loaded_settings[key])
+                        else:
+                            app_settings[key] = loaded_settings[key]
+        except (json.JSONDecodeError, IOError):
+            # In case of error, just use the default and overwrite the corrupted file later.
+            pass
+    return app_settings
 
-def save\_app\_settings(settings\_data):
-"""Saves the provided settings dictionary to the JSON file."""
-current\_settings = get\_app\_settings()
-for key, value in settings\_data.items():
-if isinstance(value, dict) and key in current\_settings and isinstance(current\_settings[key], dict):
-current\_settings[key].update(value)
-else:
-current\_settings[key] = value
-try:
-with open(SETTINGS\_FILE, 'w', encoding='utf-8') as f:
-json.dump(current\_settings, f, ensure\_ascii=False, indent=4)
-return True
-except IOError:
-return False
+def save_app_settings(settings_data):
+    """Saves the provided settings dictionary to the JSON file."""
+    current_settings = get_app_settings()
+    for key, value in settings_data.items():
+        if isinstance(value, dict) and key in current_settings and isinstance(current_settings[key], dict):
+            current_settings[key].update(value)
+        else:
+            current_settings[key] = value
+    try:
+        with open(SETTINGS_FILE, 'w', encoding='utf-8') as f:
+            json.dump(current_settings, f, ensure_ascii=False, indent=4)
+        return True
+    except IOError:
+        return False
